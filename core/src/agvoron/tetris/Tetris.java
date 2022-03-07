14d2cf77e4937762ff53b76d4ee4a6793cf1fe74
agvoron <agvoron2864@gmail.com>
2022-03-06 18:43:01 -0600

    bring in UI skin

--- core/src/agvoron/tetris/Tetris.java
+++ core/src/agvoron/tetris/Tetris.java
@@ -1,31 +1,30 @@
 package agvoron.tetris;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
-public class Tetris extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
+import agvoron.tetris.ui.TitleScreen;
+
+public class Tetris extends Game {
+    /** use to create UI elements */
+    static public Skin ui_skin;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        // TODO loading bar here - use Asset Manager?
+        ui_skin = new Skin(Gdx.files.internal("plain-james/plain-james-ui.json"));
+        this.setScreen(new TitleScreen());
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(1, 0, 0, 1);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        super.render();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        img.dispose();
+        // TODO consider strategy for disposing screens
+        ui_skin.dispose();
     }
 }
--- dev_notes.md
+++ dev_notes.md
@@ -1,5 +1,4 @@
 # === Tasks ===
-* Bring in UI skin
 * Start with moving between screens
 * Data structure for the board
 * Draw the board
