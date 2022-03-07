59858617df5e8ea849aab95c147850510fcb4b74
agvoron <agvoron2864@gmail.com>
2022-03-06 18:19:32 -0600

    libgdx app config copied in

--- desktop/src/agvoron/tetris/desktop/DesktopLauncher.java
+++ desktop/src/agvoron/tetris/desktop/DesktopLauncher.java
@@ -5,8 +5,30 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import agvoron.tetris.Tetris;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Tetris(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        // window
+        config.resizable = false;
+        config.title = "Tetris";
+//      config.addIcon(null, null); // TODO window icon
+
+        // center
+        config.x = -1;
+        config.y = -1;
+
+        // TODO graphics settings to make configurable
+        // TODO add a debug mode toggle (currently set up for testing)
+        config.width = 1080;
+        config.height = 720;
+        config.backgroundFPS = 60;
+        config.foregroundFPS = 60;
+        config.fullscreen = false;
+        config.undecorated = false;
+//      config.samples = 0;
+//      config.vSyncEnabled = true;
+        config.pauseWhenMinimized = true;
+
+        new LwjglApplication(new Tetris(), config);
+    }
 }
